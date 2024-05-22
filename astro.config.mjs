a396e57b9e411b3a5e6bd9bfd695b52bd68950b2
toma09to <yamto0916@gmail.com>
2024-05-23 04:45:20 +0900

    I completed this tutorial!!!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://toma09to-astro-tutorial.netlify.app/"
+  site: "https://toma09to-astro-tutorial.netlify.app/",
+  integrations: [preact()]
 });
